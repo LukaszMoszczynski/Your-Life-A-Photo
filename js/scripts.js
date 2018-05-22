ad868de1c2a052b28878e48db9a8cb72b9f8d1a1
Łukasz Moszczyński <moszczynski.luk@gmail.com>
2018-05-22 20:27:04 +0200

    add jumbotron

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){       
+   //sticky navbar
+   var scroll_start = 0;  
+   var startchange = $('#startchange');
+   var offset = startchange.offset();
+    if (startchange.length){
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $(".navbar-default").css('background-color', '#000000');
+          $(".navbar-default").css('padding-top', '0');
+       } else {
+          $('.navbar-default').css('background-color', 'transparent');
+          $(".navbar-default").css('padding-top', '25px');
+       }
+   });
+    }
+});
